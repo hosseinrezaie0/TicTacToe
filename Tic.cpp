0f887ab4beb997400b3d126318c34eb6fbb9d2a5
Hossein Rezaie <hossein.rezaie81@gmail.com>
2023-05-06 01:47:14 +0430

    print the board in the beginning

--- Tic.cpp
+++ Tic.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include<iomanip>
+#include<stdlib.h>
+using namespace std;
 
+char board[10] = {'o','1','2','3','4','5','6','7','8','9'};
+
+//print the board
+void print_borad(char board[]){
+
+    cout << setw(10) <<"Tic-Tac-Toe" << endl;
+
+    cout << board[1] << "  |  " << board[2] << "  |  " << board[3] << endl;
+    cout << "---------------" <<endl;
+    cout << board[4] << "  |  " << board[5] << "  |  " << board[6] << endl;
+    cout << "---------------" <<endl;
+    cout << board[7] << "  |  " << board[8] << "  |  " << board[9] << endl;
+    cout << "---------------" <<endl;
+}
+int main(){
+}
